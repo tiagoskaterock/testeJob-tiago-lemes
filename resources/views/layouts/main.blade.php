eb44b0eab6ae44250985588295c5fd255f17e12b
Tiago Bordin <tiagolemespalhano@gmail.com>
2025-03-05 22:15:52 -0300

    Feat: Layout organizado

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,17 +2,6 @@
 
 @include('layouts.aside')
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+@yield('content')
 
-    <!-- Main content -->
-    <section class="content">
-
-      <h1 class="text-center">TESTE</h1>
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  @include('layouts.footer')
+@include('layouts.footer')
--- resources/views/start/index.blade.php
+++ resources/views/start/index.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.main')
+
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+  <!-- Main content -->
+  <section class="content">
+
+    <h1 class="text-center">START</h1>
+
+  </section>
+  <!-- /.content -->
+</div>
+  <!-- /.content-wrapper -->
+
+@endsection
